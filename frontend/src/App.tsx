d4a659f8069fc211246e4195cee1adc58a160910
Caio Rezende <caiocezar50@outlook.com>
2021-05-04 17:58:33 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import { Fragment } from "react";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <Fragment>
+      <NavBar />
+      <div className="container">
+        <h2 className="text-primary">Olá Mundo!</h2>
+        <DataTable/>
+      </div>
+      <Footer />
+    </Fragment>
   );
 }
 
