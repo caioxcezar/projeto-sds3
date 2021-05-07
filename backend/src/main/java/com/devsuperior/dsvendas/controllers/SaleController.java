7aca0264cf97b7a060975160f9fa5ad89a12b4f8
Caio Rezende <caiocezar50@outlook.com>
2021-05-07 18:28:18 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,18 +1,21 @@
 package com.devsuperior.dsvendas.controllers;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 import com.devsuperior.dsvendas.service.SaleService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/Sales")
+@RequestMapping("/sales")
 public class SaleController {
     private final SaleService service;
     private final SellerRepository sellerRepository;
@@ -23,9 +26,18 @@ public class SaleController {
     }
 
     @GetMapping
-    @Transactional(readOnly = true)
     public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
         sellerRepository.findAll();
         return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+    @GetMapping("/amount-by-seller")
+    public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+        return ResponseEntity.ok(service.amountGroupedBySeller());
+    }
+
+    @GetMapping("/success-by-seller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+        return ResponseEntity.ok(service.successGroupedBySeller());
     }
 }
--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -1,10 +1,15 @@
 package com.devsuperior.dsvendas.service;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class SaleService {
@@ -14,7 +19,19 @@ public class SaleService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true)
     public Page<SaleDTO> findAll(Pageable pageable) {
         return repository.findAll(pageable).map(s -> new SaleDTO(s));
     }
+
+    @Transactional(readOnly = true)
+    public List<SaleSumDTO> amountGroupedBySeller() {
+        return repository.amountGroupedBySeller();
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSuccessDTO> successGroupedBySeller() {
+        return repository.successGroupedBySeller();
+    }
+
 }
